1fdf3c0c71fc3d14ffc88aac5ee02266769a555a
kmlx2 <kiddavid180@gmail.com>
2012-01-06 16:19:13 -0500

    Update mod_form.php

--- mod_form.php
+++ mod_form.php
@@ -29,45 +29,44 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->dirroot.'/course/moodleform_mod.php');
+require_once ('moodleform_mod.php');
 
 /**
  * Module instance settings form
  */
-class mod_newmodule_mod_form extends moodleform_mod {
+class mod_ejemplo_mod_form extends moodleform_mod {
 
     /**
      * Defines forms elements
      */
     public function definition() {
 
-        $mform = $this->_form;
+        global $COURSE;
+        $mform =& $this->_form;
 
         //-------------------------------------------------------------------------------
         // Adding the "general" fieldset, where all the common settings are showed
         $mform->addElement('header', 'general', get_string('general', 'form'));
 
         // Adding the standard "name" field
-        $mform->addElement('text', 'name', get_string('newmodulename', 'newmodule'), array('size'=>'64'));
-        if (!empty($CFG->formatstringstriptags)) {
-            $mform->setType('name', PARAM_TEXT);
-        } else {
-            $mform->setType('name', PARAM_CLEAN);
-        }
+        $mform->addElement('text', 'name', get_string('ejemploname', 'ejemplo'), array('size'=>'64'));
+        $mform->setType('name', PARAM_TEXT);
         $mform->addRule('name', null, 'required', null, 'client');
-        $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
-        $mform->addHelpButton('name', 'newmodulename', 'newmodule');
 
-        // Adding the standard "intro" and "introformat" fields
-        $this->add_intro_editor();
-
+        // Adding the optional "intro" and "introformat" pair of fields
+        $mform->addElement('htmleditor', 'intro', get_string('ejemplointro', 'ejemplo'));
+        $mform->setType('intro', PARAM_RAW);
+        $mform->addRule('intro', get_string('required'), 'required', null, 'client');
+        $mform->setHelpButton('intro', array('writing', 'richtext'), false, 'editorhelpbutton');
+   
         //-------------------------------------------------------------------------------
         // Adding the rest of newmodule settings, spreeading all them into this fieldset
         // or adding more fieldsets ('header' elements) if needed for better logic
         $mform->addElement('static', 'label1', 'newmodulesetting1', 'Your newmodule fields go here. Replace me!');
 
-        $mform->addElement('header', 'newmodulefieldset', get_string('newmodulefieldset', 'newmodule'));
-        $mform->addElement('static', 'label2', 'newmodulesetting2', 'Your newmodule fields go here. Replace me!');
+        $mform->addElement('header', 'ejemploheader', get_string('ejemploheader', 'ejemplo'));
+        $mform->addElement('text', 'campotexto', get_string('ejemplofieldset', 'ejemplo'),
+                array('size'=>'64'));
 
         //-------------------------------------------------------------------------------
         // add standard elements, common to all modules
